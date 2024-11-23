46af41167c59c0a0a91176264296d7128a8e53e2
Ali Derraj <aderraj@e2r8p7.1337.ma>
2024-11-23 22:14:02 +0100

    Makefile merged

--- Makefile
+++ Makefile
@@ -1,42 +1,64 @@
+# Colors for output
+CYAN		:=	\033[36m
+BOLD		:=	\033[1m
+GREEN		:=	\033[32m
+MAGENTA		:=	\033[35m
+END			:=	\033[0m
+
 # Compiler and flags
 CC = cc
-CFLAGS = -Wall -Werror -Wextra #-fsanitize=address -g3
-
+CFLAGS = -Wall -Werror -Wextra -fsanitize=address -g3
+# MAKEFLAGS += --no-print-directory
+# Directories
+OBJS_DIR = objs
 
 # Source files
-SRCS =	LEXER/lexer.c LEXER/node.c LEXER/utils.c\
-        PARSER/expand_utils.c PARSER/expand.c PARSER/ft_free.c PARSER/extra_utils.c PARSER/parser.c PARSER/wildcard_utils.c PARSER/wildcard.c PARSER/tree.c\
-		BUILTIN/cd_f.c BUILTIN/echo_f.c BUILTIN/env_f.c BUILTIN/env_node.c BUILTIN/pwd_f.c BUILTIN/export_f.c BUILTIN/unset_f.c BUILTIN/utils.c BUILTIN/utils1.c BUILTIN/exit_f.c\
-		EXECUTION/exec.c EXECUTION/command_utils.c EXECUTION/error_utils.c EXECUTION/exec_utils.c EXECUTION/pipe_utils.c EXECUTION/redirections_utils.c\
-		main.c
+LEXER_SRCS = LEXER/lexer.c LEXER/node.c LEXER/utils.c
+PARSER_SRCS = PARSER/expand.c PARSER/expand_utils.c PARSER/expand_utils2.c PARSER/parser.c PARSER/ft_free.c\
+			PARSER/tree.c PARSER/wildcard.c PARSER/wildcard_utils.c PARSER/parser_utils.c
+EXECUTER_SRCS = BUILTIN/cd_f.c BUILTIN/echo_f.c BUILTIN/env_f.c BUILTIN/env_node.c BUILTIN/pwd_f.c BUILTIN/export_f.c BUILTIN/unset_f.c BUILTIN/utils.c BUILTIN/utils1.c BUILTIN/exit_f.c\
+				EXECUTION/exec.c EXECUTION/command_utils.c EXECUTION/error_utils.c EXECUTION/exec_utils.c EXECUTION/pipe_utils.c EXECUTION/redirections_utils.c\
+				main.c
+SRCS = $(LEXER_SRCS) $(PARSER_SRCS) $(EXECUTER_SRCS)
+
 # Object files
-OBJS = $(SRCS:.c=.o)
+OBJS = $(patsubst %.c, $(OBJS_DIR)/%.o, $(SRCS))
 
 # Output executable
 NAME = minishell
 
-# Libft directory and library
-LIBFT_DIR = libft
-LIBFT = $(LIBFT_DIR)/libft.a
+# Libft
+LIBFTPATH = libft
+LIBFT = $(LIBFTPATH)/libft.a
 
 # Targets
 all: $(NAME)
 
 $(NAME): $(OBJS) $(LIBFT)
-	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) -lreadline -o $@
+	@$(CC) $(OBJS) $(CFLAGS) -lreadline $(LIBFT) -o $@
+	@printf "$(GREEN)$(BOLD)> $(NAME) made successfully.$(END)\n"
 
-$(LIBFT):
-	make -C $(LIBFT_DIR)
+$(OBJS_DIR)/%.o: %.c Makefile includes/minishell.h
+	@mkdir -p $(OBJS_DIR)
+	@mkdir -p $(OBJS_DIR)/LEXER $(OBJS_DIR)/PARSER
+	@$(CC) $(CFLAGS) -c $< -o $@ 
+	@printf "$(CYAN) > Compilation :$(END) $<\r"
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+$(LIBFT): $(LIBFTPATH)
+	@make -C $(LIBFTPATH)
 
 clean:
-	rm -f $(OBJS)
-	make -C $(LIBFT_DIR) clean
+	@make -C $(LIBFTPATH) clean 
+	@rm -rf $(OBJS_DIR)
+	@printf "$(CYAN)=> removed objs successfully.$(END)\n"
 
 fclean: clean
-	rm -f $(NAME)
-	make -C $(LIBFT_DIR) fclean
+	@rm -f $(NAME)
+	@rm -f $(LIBFT)
+	@printf "$(CYAN)=> removed $(LIBFT) successfully.$(END)\n"
+	@printf "$(CYAN)=> removed $(NAME) successfully.$(END)\n"
 
 re: fclean all
+
+.PHONY: all libft clean fclean re
+# .SILENT:
--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anamella <anamella@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: aderraj <aderraj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/22 22:45:57 by anamella          #+#    #+#             */
-/*   Updated: 2024/11/23 01:16:16 by anamella         ###   ########.fr       */
+/*   Created: 2024/08/16 19:52:14 by aderraj           #+#    #+#             */
+/*   Updated: 2024/11/23 20:03:22 by aderraj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,16 @@
 
 # include "../libft/libft.h"
 # include <dirent.h>
-# include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
 
 # define EXIT_STATUS 0
 # define RED "\x1b[31m"
 # define GREEN "\x1b[32m"
-
 # define BLUE "\x1b[34m"
 # define YELLOW "\x1b[33m"
 # define MAGENTA "\x1b[35m"
@@ -38,24 +33,6 @@
 # define BOLD "\x1b[1m"
 # define ITALIC "\x1b[3m"
 # define UNDERLINE "\x1b[4m"
-
-typedef struct s_mini
-{
-	struct s_env	*env;
-	struct s_tree	*root;
-	struct s_list	*list;
-	char			**char_env;
-	int				exit;
-	int				infd;
-	int				outfd;
-}					t_mini;
-
-typedef struct s_env
-{
-	char			*var;
-	char			*val;
-	struct s_env	*next;
-}					t_env;
 
 typedef enum e_token
 {
@@ -75,7 +52,6 @@ typedef struct s_redir
 {
 	t_token			mode;
 	char			*file;
-	int				fd;
 	struct s_redir	*next;
 }					t_redir;
 
@@ -106,6 +82,7 @@ typedef struct s_expand
 	int				res_idx;
 	int				res_size;
 	int				quotes_flags[2];
+	bool			to_split;
 	bool			to_sort;
 	t_list			*idx_node;
 }					t_expand;
@@ -171,60 +148,16 @@ t_list				*insert_node(t_list *start, t_list *new_node);
 void				sort_fnames(t_list *start, t_list *end);
 char				*construct_filename(t_wildcard *rules, char *s);
 void				set_position(t_tree *stats[]);
-void				arrange_nodes(t_list *list[3], t_redir *redirections);
+void				arrange_nodes(t_list *list[3], t_redir **redirections);
 void				merge_nodes(t_list *list, t_redir *redirs);
 t_list				*get_redirections(t_list *list, t_list *current,
 						t_redir **redirect);
+void				expand_exit_status(t_expand *params);
+void				append_node(t_redir **redirection, t_redir *new);
+bool				innormal_var(t_expand *params);
 void				parser(t_list *list);
 void				print_ast(t_tree *node, int level);
-void				set_redirections(t_list *node, t_list *last_parenthesis);
+void				split_node(t_list *node);
+char				*get_varname(char *s, int *j);
 
-// env and builtin function
-t_env				*create_env(char **env);
-t_env				*new_env(char *s, int *status);
-int					add_env(t_env **env, t_env *new_env);
-void				del_env(t_env **env, char *var);
-void				free_env(t_env *env);
-char				*get_val(char *s, int *status);
-char				*get_var(char *s, int *status);
-char				*get_env(char *name, t_env *env);
-int					set_env(char *var, char *val, t_env **env);
-int					export_f(char **env, t_mini *mini);
-int					unset_f(char **env, t_mini *mini);
-int					env_f(t_env *env, int flag);
-int					echo_f(char **args, t_mini *mini);
-int					cd_f(char **path, t_mini *mini);
-int					pwd_f(char **args, t_mini *mini);
-int					exit_f(char **args, t_mini *mini);
-int					cd_free(void *p, int f);
-// end of env and builtin funcion
-
-// execution function
-int					execute_command(t_tree *root, t_mini *mini);
-int					execute_and(t_tree *root, t_mini *mini);
-int					execute_or(t_tree *root, t_mini *mini);
-int					execute_parenthesis(t_tree *root, t_mini *mini);
-int					execute_pipe(t_tree *node, t_mini *mini);
-char				*path_join(char *path, char *cmd);
-int					check_path(char *cmd, int *status);
-int					join_command(t_tree *root, t_env **env, int *status);
-int					check_builtin(t_tree *root, t_mini *mini, int *exit);
-int					ft_strcmp(char *s1, char *s2);
-void				cmd_error(char *cmd);
-void				error_msg(char *msg, t_mini *mini, int status);
-void				free_(char **arr);
-void				free_exit(t_mini *mini, int status);
-int					*create_pipe(int *pipefd, t_mini *mini);
-void				close_pipe(int *pipefd);
-int					end_of_pipe(pid_t pid_left, pid_t pid_right, int *pipefd);
-int					close_fd(int fd1, int fd2);
-int					redirections_type(t_redir *redirections, t_mini *mini);
-int					check_redirection(t_tree *root, t_mini *mini);
-int					count_env(t_env *tmp);
-char				**create_char_env(t_env *env);
-int					heredoc(const char *delimiter, t_mini *mini);
-int					execute_ast(t_tree *root, t_mini *mini);
-void				child_process1(t_tree *root, int *pipefd, t_mini *mini);
-void				child_process2(t_tree *root, int *pipefd, t_mini *mini);
-// end of execution function
 #endif
