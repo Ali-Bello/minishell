f538b7da64fe79ecde50023d19b1ba2e445cb054
Anas Amellah <anamella@c4r9p4.1337.ma>
2024-11-29 17:29:21 +0100

    fix exit <long max>

--- BUILTIN/export_f.c
+++ BUILTIN/export_f.c
@@ -6,7 +6,7 @@
 /*   By: anamella <anamella@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 22:44:57 by anamella          #+#    #+#             */
-/*   Updated: 2024/11/28 19:16:59 by anamella         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:23:40 by anamella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,17 @@ void	export_error(char *name)
 
 int	check_var(char *name)
 {
-	int	len;
+	int	i;
 
-	len = ft_strlen(name) - 1;
+	i = 0;
 	if (!ft_isalpha(name[0]) && name[0] != '_')
 		return (export_error(name), 1);
-	if (!ft_isalpha(name[len]) && name[len] != '_' && !ft_isdigit(name[len]))
-		return (export_error(name), 1);
+	while (name[i])
+	{
+		if (!ft_isalpha(name[i]) && !ft_isdigit(name[i]) && name[i] != '_')
+			return (export_error(name), 1);
+		i++;
+	}
 	return (0);
 }
 
@@ -72,7 +76,8 @@ int	export_f(char **env, t_mini *mini)
 	exit_status = 0;
 	while (env && env[i])
 	{
-		exit_status = check_and_add(env[i], env_l);
+		if (check_and_add(env[i], env_l))
+			exit_status = 1;
 		i++;
 	}
 	return (exit_status);
--- PARSER/errors.c
+++ PARSER/errors.c
@@ -6,9 +6,30 @@
 /*   By: aderraj <aderraj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 05:05:39 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/27 19:08:25 by aderraj          ###   ########.fr       */
+/*   Updated: 2024/11/29 02:54:29 by aderraj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+bool    check_ambiguous_redirect(t_list *list)
+{
+    t_redir *tmp;
+
+    while (list)
+    {
+        tmp = list->data.redirections;
+        while (tmp)
+        { 
+            if (tmp->ambiguous)
+            {
+                ft_putstr_fd(tmp->file, 2);
+                ft_putstr_fd(": ambiguous redirect\n", 2);
+                return (true);
+            }
+            tmp = tmp->next;
+        }
+        list = list->next;
+    }
+    return (false);
+}
